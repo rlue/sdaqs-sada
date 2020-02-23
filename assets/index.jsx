2bcb21e80cd3b857e1f07674da0bb6e76537d742
Ryan Lue <hello@ryanlue.com>
2020-02-23 23:48:23 +0800

    Fix JS style errors

---
+++ assets/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+function App() {
+  return (
+    <div className="container">
+      hello
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
--- config/webpack.js
+++ config/webpack.js
@@ -3,34 +3,26 @@ const PnpWebpackPlugin = require('pnp-webpack-plugin')
 
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: './assets/index.js',
+  entry: './assets/index.jsx',
   module: {
     rules: [
       { test: /\.svg$/, use: 'svg-inline-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-      { test: /\.js$/,  use: 'babel-loader' }
-    ]
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.jsx?$/, use: 'babel-loader' },
+    ],
   },
   output: {
     path: path.resolve('public', 'assets'),
-    filename: 'index.js'
+    filename: 'index.js',
   },
   resolve: {
-    extensions: [
-      '.js',
-      '.jsx'
-    ],
-    plugins: [
-      PnpWebpackPlugin
-    ]
+    extensions: ['.js', '.jsx'],
+    plugins: [PnpWebpackPlugin],
   },
   resolveLoader: {
-    plugins: [
-      PnpWebpackPlugin.moduleLoader(module)
-    ]
+    plugins: [PnpWebpackPlugin.moduleLoader(module)],
   },
   devServer: {
-    writeToDisk: true
-  }
+    writeToDisk: true,
+  },
 }
-
