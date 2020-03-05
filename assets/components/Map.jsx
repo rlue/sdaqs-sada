226b91f0e0572b580dca881bc026d0c3e95e80e1
Ryan Lue <hello@ryanlue.com>
2020-03-05 08:20:22 +0800

    Refactor effects into named functions

--- assets/components/Map.jsx
+++ assets/components/Map.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import mapboxgl from 'mapbox-gl'
 
 mapboxgl.accessToken =
   'pk.eyJ1Ijoicmx1ZSIsImEiOiJjazZwOHIwdXcwNzg1M2xuejVkbGNkaGEwIn0.S0KbmonSFTp9xI5J2ZGANQ'
 
-export default function Map({ matches }) {
-  const mapContainer = React.useRef()
-  const map = React.useRef()
-  const markers = React.useRef([])
+export default function Map({ matches, selection }) {
+  const mapContainer = useRef()
+  const map = useRef()
+  const markers = useRef([])
+  const popup = useRef()
 
-  React.useEffect(() => {
+  useMap(map, mapContainer)
+  useMarkers(map, matches, markers)
+
+  return <div className="item__bifold-right" ref={mapContainer} />
+}
+
+Map.propTypes = {
+  matches: PropTypes.arrayOf(
+    PropTypes.shape({
+      latitude: PropTypes.string,
+      longitude: PropTypes.string,
+    }),
+  ).isRequired,
+}
+
+function useMap(map, mapContainer) {
+  useEffect(() => {
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
@@ -25,8 +42,10 @@ export default function Map({ matches }) {
 
     return () => map.current.remove()
   }, [])
+}
 
-  React.useEffect(() => {
+function useMarkers(map, matches, markers) {
+  useEffect(() => {
     markers.current = matches.map(({ longitude, latitude }) =>
       new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map.current),
     )
@@ -44,15 +63,4 @@ export default function Map({ matches }) {
       markers.current.forEach((marker) => marker.remove())
     }
   }, [matches])
-
-  return <div className="item__bifold-right" ref={mapContainer} />
-}
-
-Map.propTypes = {
-  matches: PropTypes.arrayOf(
-    PropTypes.shape({
-      latitude: PropTypes.string,
-      longitude: PropTypes.string,
-    }),
-  ).isRequired,
 }
