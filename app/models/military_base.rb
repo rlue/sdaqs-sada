dfb1b36d03f5265eabd345a4f270437efd81a0ed
Ryan Lue <hello@ryanlue.com>
2020-06-23 23:14:36 +0800

    dev: Prep for new DB config/seeding setup

--- app/models/military_base.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class MilitaryBase < Sequel::Model
-  def validate
-    super
-    errors.add(:lat, 'must be between -90 and 90') unless (-90..90).include?(lat)
-    errors.add(:lng, 'must be between -180 and 180') unless (-180..180).include?(lng)
-  end
-end
