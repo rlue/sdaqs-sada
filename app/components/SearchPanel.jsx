rlue/sdaqs-sada

commit 78a94b4f4fbf4d6d4ea1937299a9ead50dd9da14

rfg: Namespace CSS animation classes (and...)
This commit refactors CSS animation rules in the following ways:

1. Break shorthands into their individual rules:

       # before
       animation: slide-right 500ms ease-in-out forwards;

       # after
       animation-name: slide-right;
       animation-duration: 500ms;
       animation-timing-function: ease-in-out;
       animation-fill-mode: forwards;

   Even though we still prefer shorthands for `border` rules,
   we opted to break them apart for animations,
   since they support a _lot_ of parameters (`animation-x` rules),
   and being more explicit about the ones we use makes debugging easier.

2. Separate `animation-name` rules from all other animation parameters:

   # before
   .slide-right {
     animation: slide-right 500ms ease-in-out forwards;
   }

   # after
   .slide-right {
     animation-name: slide-right;
   }

   .content-window {
     animation-duration: 500ms;
     animation-timing-function: ease-in-out;
     animation-fill-mode: forwards;
   }

   If these parameters are not part of
   the animation's core definition (@keyframes rule),
   they should not be attached to the only CSS hook for that animation.
   This way, two different elements can animate the same @keyframes
   with different speeds/delays/counts/etc.

   (They were originally bundled together
   because that's how the animate.css library does it,
   and that's where these effects were borrowed from.)

3. Namespace CSS animation classes:

       # before
       .slide-right { ... }

       # after
       .animate-slide-right { ... }

   Why? All CSS classes live in a global namespace,
   and patterns like this help clarify intention in the codebase.

   Also, we had a `.float` class
   for a `@keyframes float { ... }` animation,
   and we didn't want it to be confused with the `float:` CSS rule.

app/components/SearchPanel.jsx
@@ -40,17 +40,10 @@ export default function SearchPanel({
         <button
           type="button"
           className={classNames(
-            'hidden',
-            'absolute',
-            'w-8',
-            'h-8',
-            'z-10',
-            'rounded-full',
-            'bg-white',
-            'shadow-md',
+            'search-panel__back-button',
             {
-              'zoom-enter': userFlow.mode === 'exposure report',
-              'zoom-exit': userFlow.mode === 'deployment builder',
+              'animate-zoom-enter': userFlow.mode === 'exposure report',
+              'animate-zoom-exit': userFlow.mode === 'deployment builder',
             },
           )}
           onClick={() => setUserFlow({ mode: 'deployment builder' })}

assets/index.css
@@ -92,6 +92,14 @@
                 1px  1px 3px hsla(48, 100%, 50%, 0.2);
 }
 
+.search-panel__back-button {
+  @apply block hidden absolute w-8 h-8 z-10 rounded-full bg-white shadow-md;
+
+  animation-duration: 300ms;
+  animation-timing-function: ease-out;
+  animation-fill-mode: forwards;
+}
+
 /* Delete button ------------------------------------------------------------ */
 
 .anticon-delete {
@@ -142,7 +150,11 @@ input:focus,
 
 .content-window {
   @apply flex h-full overflow-hidden;
+
   width: 200vw;
+  animation-duration: 500ms;
+  animation-timing-function: ease-in-out;
+  animation-fill-mode: forwards;
 }
 
 /* MAP ====================================================================== */
@@ -192,7 +204,10 @@ svg.map-pin > text {
 }
 
 svg.map-pin--highlighted {
-  animation: float 1s ease-in-out infinite;
+  animation-name: float;
+  animation-duration: 1s;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: infinite;
 }
 
 /* Pin labels --------------------------------------------------------------- */
@@ -256,8 +271,8 @@ svg.map-pin--highlighted {
   }
 }
 
-.slide-right {
-  animation: slide-right 500ms ease-in-out forwards;
+.animate-slide-right {
+  animation-name: slide-right;
 }
 
 @keyframes slide-left {
@@ -270,8 +285,8 @@ svg.map-pin--highlighted {
   }
 }
 
-.slide-left {
-  animation: slide-left 500ms ease-in-out forwards;
+.animate-slide-left {
+  animation-name: slide-right;
 }
 
 @keyframes zoom-enter {
@@ -290,9 +305,8 @@ svg.map-pin--highlighted {
   }
 }
 
-.zoom-enter {
-  animation: zoom-enter 300ms ease-out forwards;
-  display: block;
+.animate-zoom-enter {
+  animation-name: zoom-enter;
 }
 
 @keyframes zoom-exit {
@@ -312,7 +326,6 @@ svg.map-pin--highlighted {
   }
 }
 
-.zoom-exit {
-  animation: zoom-exit 300ms ease-out forwards;
-  display: block;
+.animate-zoom-exit {
+  animation-name: zoom-exit;
 }

app/App.jsx
@@ -51,8 +51,8 @@ export default function App() {
         className={classNames(
           'content-window',
           {
-            'slide-right': userFlow.mode === 'deployment builder',
-            'slide-left': userFlow.mode === 'exposure report',
+            'animate-slide-right': userFlow.mode === 'deployment builder',
+            'animate-slide-left': userFlow.mode === 'exposure report',
           },
         )}
       >

commit 44e0db700d4b7e78d85e0da466af8795a85a8652

uix: Make first tab-focus go to search input
78a94b4 introduced a regression in the UI
in which the user must hit Tab _twice_ after page load
for focus to go to the search input.

This was the result of changes in
`display` and `visibility` CSS rules on .search-panel__back-button,
which comes before the first search input in the DOM.

The fix was to re-apply the correct visibility rules on the button.
`transition-delay` rules had to be adjusted
to ensure the show/hide animation would not be cut short
by an immediate toggling of visibility.

app/components/SearchPanel.jsx
@@ -41,12 +41,9 @@ export default function SearchPanel({
           type="button"
           className={classNames(
             'search-panel__back-button',
-            'transform',
             {
-              'scale-1': userFlow.mode === 'chart',
-              'opacity-100': userFlow.mode === 'chart',
-              'scale-0': userFlow.mode === 'map',
-              'opacity-0': userFlow.mode === 'map',
+              'search-panel__back-button--shown': userFlow.mode === 'chart',
+              'search-panel__back-button--hidden': userFlow.mode === 'map',
             },
           )}
           onClick={() => setUserFlow({ mode: 'map' })}

assets/index.css
@@ -95,10 +95,25 @@
 .search-panel__back-button {
   @apply block absolute w-8 h-8 z-10 rounded-full bg-white shadow-md;
 
-  transition-property: transform, opacity;
-  transition-duration: 300ms;
+  transition-property: visibility, transform, opacity;
+  transition-duration: 0s, 300ms, 300ms;
   transition-timing-function: ease-out;
-  transition-delay: 0s, 150ms;
+}
+
+.search-panel__back-button--shown {
+  visibility: visible;
+  transform: scale3d(1, 1, 1);
+  opacity: 1;
+
+  transition-delay: 0s, 0s, 150ms;
+}
+
+.search-panel__back-button--hidden {
+  visibility: hidden;
+  transform: scale3d(0.3, 0.3, 0.3);
+  opacity: 0;
+
+  transition-delay: 300s, 0s, 150ms;
 }
 
 /* Delete button ------------------------------------------------------------ */