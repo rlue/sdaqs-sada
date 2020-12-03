8b5f2b85cf1b4ed6527d178cff7c096f89876f97
Ryan Lue <hello@ryanlue.com>
2020-12-03 15:13:22 +0800

    rfg: Remove cruft (never-used React component)

--- app/components/Icons.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import * as Icons from './Icons';
-
-test('Totally meaningless sample test', () => {
-  const wrapper = shallow(<Icons.MapPin className="foo" onClick={() => {}} />);
-
-  expect(wrapper.find('svg').length).toBe(1);
-});
