fab3a6d942a9afdfdfd891873d384a0d0bad699a
Ryan Lue <hello@ryanlue.com>
2020-12-03 15:08:38 +0800

    rfg: Replace raw CSS with Tailwind
    This application's pre-flight WebGL / Mapbox API key validation warnings
    are displayed dead center against a plain, white background.
    
    Centering content both vertically and horizontally
    used to be a real headache in CSS.
    This commit changes the approach to use flexbox instead:
    
        /* before */
        position: absolute;
        top: 50%;
        left: 50%;
        transform: translate(-50%, -50%);
    
        /* after */
        display: flex;
        height: 100vh;
        align-items: center;
        width: 100vw; /* was this even necessary?? */
        justify-content: center;
    
    These new properties are all available in stock Tailwind CSS,
    so this commit moves these rules from a `style` attr to `className`.

--- app/components/preflight/MapboxSupportCheck.jsx
+++ app/components/preflight/MapboxSupportCheck.jsx
@@ -6,13 +6,6 @@ import mapboxgl from 'mapbox-gl';
 export default function MapboxSupportCheck({ children }) {
   const [tokenValidity, setTokenValidity] = useState(null);
 
-  const cssCentered = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-  };
-
   useEffect(() => {
     fetch(`https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/1/0/0.mvt?access_token=${MAPBOXGL_ACCESS_TOKEN}`)
       .then((response) => {
@@ -22,33 +15,41 @@ export default function MapboxSupportCheck({ children }) {
 
   if (!mapboxgl.supported()) {
     return (
-      <div style={cssCentered}>
-        <h1 className="text-5xl mb-4">
-          <span role="img" aria-label="Browser Error">🤦</span>
-          {' '}
-          D’oh! Your browser does not support Mapbox.
-        </h1>
-        <p className="text-xl">
-          Find out more at
-          {' '}
-          <a href="https://get.webgl.org/" rel="noopener">https://get.webgl.org</a>
-          .
-        </p>
+      <div className="flex items-center justify-center h-screen mx-4">
+        <div>
+          <h1 className="text-5xl mb-4">
+            <span role="img" aria-label="Browser Error">🤦</span>
+            {' '}
+            D’oh! Your browser does not support Mapbox.
+          </h1>
+          <p className="text-xl">
+            Find out more at
+            {' '}
+            <a href="https://get.webgl.org/" rel="noopener">https://get.webgl.org</a>
+            .
+          </p>
+        </div>
       </div>
     );
   } else if (tokenValidity === null) {
-    return <Spin size="large" style={cssCentered} />;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Spin size="large" />
+      </div>
+    )
   } else if (tokenValidity === false) {
     return (
-      <div style={cssCentered}>
-        <h1 className="text-5xl mb-4">
-          <span role="img" aria-label="Application Error">🤦</span>
-          {' '}
-          D’oh! This site was built with an invalid Mapbox access token.
-        </h1>
-        <p className="text-xl">
-          Please contact your administrator.
-        </p>
+      <div className="flex items-center justify-center h-screen mx-4">
+        <div>
+          <h1 className="text-5xl mb-4">
+            <span role="img" aria-label="Application Error">🤦</span>
+            {' '}
+            D’oh! This site was built with an invalid Mapbox access token.
+          </h1>
+          <p className="text-xl">
+            Please contact your administrator.
+          </p>
+        </div>
       </div>
     );
   } else {
