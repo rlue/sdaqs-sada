d681b0b5a1348e513c230657b8276eb678bb185d
Ryan Lue <hello@ryanlue.com>
2020-12-30 02:09:16 +0800

    bfx: Show base name (not ID) in chart legends
    Prior to this commit, chart legends would identify bases
    by ID (e.g., VA0001) rather than name (e.g., Camp Krusty).
    
    Going by ID eliminates ambiguity
    (base names are not guaranteed to be unique),
    but users are not expected to recognize/distinguish bases by ID anyway.

--- app/components/Chart.jsx
+++ app/components/Chart.jsx
@@ -2,16 +2,17 @@ import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import {Chart as ChartJS} from 'chart.js';
 import 'chartjs-plugin-colorschemes';
+import sites from '../../data/sites.json';
 
 // NOTE the difference:
-// this `exposures` var            -> exposures[base_name][date]
-// top-level `exposures` state var -> exposures[contaminant][base_name][date]
+// this `exposures` var            -> exposures[base_id][date]
+// top-level `exposures` state var -> exposures[contaminant][base_id][date]
 function exposuresToChartJSDataset(exposures) {
   return {
     datasets: Object.keys(exposures)
-      .map((baseName) => ({
-        label: baseName,
-        data: Object.entries(exposures[baseName])
+      .map((baseId) => ({
+        label: sites[baseId].name,
+        data: Object.entries(exposures[baseId])
           .map(([date, concentration]) => ({
             x: date,
             y: concentration,
