rlue/sdaqs-sada

commit 8a0a7ebd18cde70dcf6aeb1e20f6029ad97d711c

dep: Upgrade dependencies

Gemfile
@@ -2,7 +2,7 @@
 
 source 'https://rubygems.org'
 
-ruby '2.7.2'
+ruby '3.0.0'
 
 gem 'activesupport', '~> 6.0'
 gem 'anbt-sql-formatter', '~> 0.1', require: 'anbt-sql-formatter/formatter'

.ruby-version
@@ -1,1 +1,1 @@
-2.7.2
+3.0.0

commit 33935f7354a32b0b683d5ee42b1983cf2f445395

dep: Fix app config broken in 8a0a7eb
8a0a7eb incorporated upgrades to many app dependencies.
In the process, it introduced breaking changes that went undetected:

* Ruby 3.0 does not bundle WEBrick by default,
  so no web server was included for `rackup` to invoke.
  In this commit, we explicitly add WEBrick to the Gemfile.
* The upgrade to webpack v5 changed the configuration API
  for webpack-dev-server options
  (the `writeToDisk` option now belongs to webpack-dev-middleware).

Gemfile
@@ -10,6 +10,7 @@ gem 'pg', '~> 1.2'
 gem 'roda', '~> 3.29'
 gem 'sequel', '~> 5.29'
 gem 'sequel_pg', '~> 1.12', require: false
+gem 'webrick', '~> 1.7'
 
 group :development, :test do
   gem 'pry', '~> 0.13'

config/webpack.js
@@ -39,7 +39,10 @@ module.exports = {
     filename: '[name]-[contenthash].js',
   },
   plugins: [
-    new AssetsPlugin({ path: path.resolve('assets') }),
+    new AssetsPlugin({
+      path: path.resolve('assets'),
+      removeFullPathAutoPrefix: true,
+    }),
     new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ['*.js'] }),
     new webpack.DefinePlugin({
       MAPBOXGL_ACCESS_TOKEN: JSON.stringify(process.env.MAPBOXGL_ACCESS_TOKEN),
@@ -49,6 +52,8 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   devServer: {
-    writeToDisk: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
   },
 }