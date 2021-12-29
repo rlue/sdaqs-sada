b6db7a926015d45e618e497d64d29e48bc139871
Ryan Lue <hello@ryanlue.com>
2021-12-29 12:48:37 -0800

    bfx: Set ChartPage header to match current view

--- app/components/ChartPage.jsx
+++ app/components/ChartPage.jsx
@@ -2,23 +2,32 @@ import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Chart from './Chart';
 import ExposureSummary from './ExposureSummary';
+import { exposureMap } from '../utils/chartHelper';
 
 export default function ChartPage({ exposures, exposureStats, userFlow }) {
   return (
     <div className="exposure-container flex w-screen min-h-screen overflow-x-hidden">
       <div className="exposure-shim" />
       {
-        Object.keys(exposures).length
-          ? <div className="grow m-8">
-              <h1 className="text-5xl text-violet-900 uppercase mb-5">Summary</h1>
-              {userFlow.contaminant
-                ? <Chart {...{ exposures, exposureStats, userFlow }} />
-                : <ExposureSummary {...{ exposures, exposureStats }} />
-              }
-            </div>
-          : <div className="flex w-full h-screen items-center justify-center">
+        Object.keys(exposures).length === 0
+          ? <div className="flex w-full h-screen items-center justify-center">
               <div className="spinner" />
             </div>
+          : userFlow.contaminant === undefined
+            ? <div className="grow m-8">
+                <h1 className="text-6xl text-gray-800 uppercase mb-5">Summary</h1>
+                <ExposureSummary {...{ exposures, exposureStats }} />
+              </div>
+            : exposureMap[userFlow.contaminant]?.description
+              ? <div className="grow m-8">
+                  <h1 className="text-6xl text-gray-800 uppercase mb-2">{exposureMap[userFlow.contaminant].name}</h1>
+                  <h2 className="text-3xl text-gray-600 mb-5">{exposureMap[userFlow.contaminant].description}</h2>
+                  <Chart {...{ exposures, exposureStats, userFlow }} />
+                </div>
+              : <div className="grow m-8">
+                  <h1 className="text-6xl text-gray-800 uppercase mb-5">{exposureMap[userFlow.contaminant].name}</h1>
+                  <Chart {...{ exposures, exposureStats, userFlow }} />
+                </div>
       }
     </div>
   );
